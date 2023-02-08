d0a2a3219af3f40fb70e102546a1a3e030929dfd
Ridvan Ovsene <ovsene2028@hotmail.com>
2023-02-08 09:22:06 +0300

    düzenleme

--- Business/Services/AppUserService.cs
+++ Business/Services/AppUserService.cs
@@ -12,42 +12,42 @@ namespace Business.Services
 {
     public class AppUserService : IAppUserService
     {
-        private IAppUserRepository _kullaniciRepository;
+        private IAppUserRepository _appUserRepository;
         public AppUserService(IAppUserRepository kullaniciRepository)
         {
-            _kullaniciRepository = kullaniciRepository;
+            _appUserRepository = kullaniciRepository;
         }
 
         public AppUser GetById(int Id)
         {
-            return _kullaniciRepository.Get(a => a.ID == Id);
+            return _appUserRepository.Get(a => a.ID == Id);
         }
 
         public List<AppUser> GetList()
         {
-            return _kullaniciRepository.GetList().ToList();
+            return _appUserRepository.GetList().ToList();
         }
         public AppUser Login(string UserName, string Password)
         {
-            return _kullaniciRepository.Get(a => a.UserName == UserName && a.Password == Password);
+            return _appUserRepository.Get(a => a.UserName == UserName && a.Password == Password);
         }
         //public List<KullaniciDto> GetListKullanici()
         //{
-        //    return new List<KullaniciDto>(_kullaniciRepository.GetListKullanici().ToList());
+        //    return new List<KullaniciDto>(_appUserRepository.GetListKullanici().ToList());
         //}
         public string Add(AppUser appUser)
         {
-            _kullaniciRepository.Add(appUser);
+            _appUserRepository.Add(appUser);
             return "Ok";
         }
         public string Update(AppUser appUser)
         {
-            _kullaniciRepository.Update(appUser);
+            _appUserRepository.Update(appUser);
             return "Ok";
         }
         public string Delete(AppUser appUser)
         {
-            _kullaniciRepository.Delete(appUser);
+            _appUserRepository.Delete(appUser);
             return "Ok";
         }
 
